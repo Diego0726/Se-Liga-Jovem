6770cc76fabb05bdd80ec91c454bbde400cc84b0
Diego_Barreto <diebarreto5@gmail.com>
2024-07-20 11:50:44 -0300

    Apaguei muita coisa

--- Mind_in_din/metro.config.js
+++
@@ -1,6 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require('nativewind/metro');
-
-const config = getDefaultConfig(__dirname)
-
-module.exports = withNativeWind(config, { input: '../Mind_in_din/styles/global.css' })
